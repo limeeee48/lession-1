b86c635491f196b8942d74e52e23869eddc50dda
lime48 <limelime588@gmail.com>
2025-09-10 13:43:24 -0700

    Add files via upload

---
+++ Port-Scanner.sh
@@ -0,0 +1,22 @@
+
+
+case "$1" in
+	[0-9][0-9])
+		_port=$1
+		;;
+	[0-9][0-9][0-9])
+		_port=$1
+		;;
+	*)
+		echo -e "\e[1;31mPort Unknown\e[0m" ; exit 1
+esac
+#_command="echo > /dev/tcp/google.com/$_port"
+#read port1
+timeout 3 bash -c "echo > /dev/tcp/google.com/$_port" 2>/dev/null
+if [[ $? -eq 0 ]] ; then
+	echo -e "\n\e[1;32m[+] Port $_port Open\e[0m" ; exit 0
+else
+	echo -e "\n\e[1;31m[-] Port $_port Close\e[0m" ;exit 1
+fi
+
+
